2bc3693235f84f4c513b7d4b0bf5c89ed1d143d8
James Halliday <mail@substack.net>
2014-07-30 18:32:04 +1200

    example works

---
+++ example/useradd.js
@@ -0,0 +1,32 @@
+var level = require('level');
+var db = level('/tmp/users.db', { valueEncoding: 'json' });
+var batch = require('../');
+
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2), {
+    alias: { u: 'user', p: 'pass', b: 'bio' }
+});
+
+var crypto = require('crypto');
+var shasum = require('shasum');
+
+var salt = crypto.randomBytes(32);
+var hash = shasum(Buffer.concat([ salt, Buffer(argv.pass) ]));
+
+var rows = [
+    {
+        key: 'users!' + argv.user,
+        value: { bio: argv.bio }
+    },
+    {
+        key: 'login!basic!' + argv.user,
+        value: { salt: salt, hash: hash }
+    }
+];
+
+batch(db, rows, function (err) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+});
