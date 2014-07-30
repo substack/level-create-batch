4f4206e444f40f5702e543ff389072fe9c3b8dcd
James Halliday <mail@substack.net>
2014-07-30 18:14:41 +1200

    failing synthetic error test

---
+++ test/err.js
@@ -0,0 +1,24 @@
+var test = require('tape');
+var level = require('level-test')();
+var db = level('users.db-' + Math.random(), { valueEncoding: 'json' });
+var batch = require('../');
+
+db.get = function (key, cb) {
+    process.nextTick(function () {
+        var err = new Error('blah');
+        err.code = 'ENOBLAH';
+        cb(err);
+    });
+};
+
+test('database error', function (t) {
+    t.plan(1);
+    
+    var rows = [
+        { key: 'substack', value: { bio: 'beep boop' } },
+        { key: 'index', value: 3000 },
+    ];
+    batch(db, rows, function (err) {
+        t.equal(err.code, 'ENOBLAH');
+    });
+});
