ec185a9c11be8c773d5d2318e1e1ebbce58a60ae
James Halliday <mail@substack.net>
2014-07-30 17:54:29 +1200

    more thorough tests for create and put rows

--- test/put.js
+++ test/put.js
@@ -17,3 +17,27 @@ test('put', function (t) {
         });
     });
 });
+
+test('put create', function (t) {
+    t.plan(4);
+    
+    var arows = [
+        { key: 'substack', value: { bio: 'beep boop' }, type: 'create' },
+        { key: 'index', value: 3000, type: 'put' }
+    ];
+    batch(db, arows, function (err) {
+        t.equal(err.code, 'EXISTS');
+        db.get('index', function (err, res) {
+            t.ifError(err);
+            t.equal(res, 2000);
+        });
+        
+        var brows = [
+            { key: 'xxx', value: { bio: '...' }, type: 'create' },
+            { key: 'index', value: 3000, type: 'put' }
+        ];
+        batch(db, brows, function (err) {
+            t.ifError(err, "put rows don't matter");
+        });
+    });
+});
